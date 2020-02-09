0d60e665909f5fba1cf75848af0d7a0b76c41e2b
matthew rowlinson <matthewrowlinson1@gmail.com>
2020-02-09 21:18:34 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,7 +1,9 @@
 def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
-	{}
+	{:browser => {
+	  :chrome => {}, :edge => {}, :firefox => {}, :explorer => {}, :opera => {}
+	}}
 end
 
 def monopoly_with_second_tier
